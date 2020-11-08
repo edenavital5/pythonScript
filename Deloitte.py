4124441f46e801fee668ef5b88cd18758960ed39
Eden Avital <edenavital18@gmail.com>
2020-11-08 18:51:54 +0200

    m

--- Deloitte.py
+++ Deloitte.py
@@ -25,6 +25,5 @@ instances = ec2.create_instances(
     MaxCount=1,
     InstanceType='t2.micro',
     KeyName='keypair6/11/20',
-    NetworkInterfaces=[{'SubnetId': subnet.id, 'DeviceIndex': 0, 'AssociatePublicIpAddress': True}],
-    id='1'
+    NetworkInterfaces=[{'SubnetId': subnet.id, 'DeviceIndex': 0, 'AssociatePublicIpAddress': True}]
  )
